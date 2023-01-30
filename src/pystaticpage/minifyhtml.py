be65fd18155c4010b5ab42339b4265060d383825
Arnaldo Ono <git@onoarnaldo.com>
2023-01-30 17:47:29 +0100

    Fix remove comment in html minify

--- src/pystaticpage/minifyhtml.py
+++ src/pystaticpage/minifyhtml.py
@@ -21,9 +21,9 @@ def remove_repeated_spaces(lines: _.Iterable[str]) -> _.Generator[str, None, Non
         yield RE_SPACES.subn(' ', line)[0]
 
 
-RE_COMMENT_ONELINE = _re.compile(r'<!--[^-->]*-->')
-RE_COMMENT_MULTI_OPEN = _re.compile(r'<!--[^-->]*')
-RE_COMMENT_MULTI_CLOSE = _re.compile(r'[^-->]*-->')
+RE_COMMENT_ONELINE = _re.compile(r'<!--[\s\S]*-->')
+RE_COMMENT_MULTI_OPEN = _re.compile(r'<!--[\s\S]*')
+RE_COMMENT_MULTI_CLOSE = _re.compile(r'[\s\S]*-->')
 
 
 def remove_comments(lines: _.Iterable[str]) -> _.Generator[str, None, None]:
